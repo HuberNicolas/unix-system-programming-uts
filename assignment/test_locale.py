35bcefd000cc3588ba4e9d0b4edcf34309bc44da
Nicolas Huber <nicolas.huber2@uzh.ch>
2024-04-26 16:07:41 +1000

    Adding Tests

--- assignment/test_locale.py
+++ assignment/test_locale.py
@@ -59,6 +59,59 @@ class TestLocaleScript(unittest.TestCase):
             self.assertIn('Error: The file ./data/non_existent_file.csv does not exist or is not readable.',
                           sys.stdout.getvalue())
 
+    # Req. 2: Invocation Command
+    def test_correct_invocation_args(self):
+        test_args = ['locale.py', '-a', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args):
+            my_locale.main()
+            self.assertIn('Available locales:\nen_US\nfr_FR\n', sys.stdout.getvalue())
+
+    # Req. 3: File Format
+
+    # Req. 4: Option -a
+    def test_order_of_locales(self):
+        test_args = ['locale.py', '-a', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args):
+            my_locale.main()
+            self.assertEqual(sys.stdout.getvalue(), "Available locales:\nen_US\nfr_FR\n")
+
+    # Req. 5: Option -m
+    def test_charmaps_mixed_with_locales(self):
+        test_args = ['locale.py', '-m', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args):
+            my_locale.main()
+            self.assertEqual(sys.stdout.getvalue(), "Available charmaps:\nISO-8859-1\nGBK\n")
+
+    # Req. 6: Option -l [language]
+    def test_multiple_locales_charmaps_one_language(self):
+        test_args = ['locale.py', '-l', 'English', './data/language_specific_english.csv']
+        with patch('sys.argv', test_args):
+            my_locale.main()
+            self.assertIn("Language English:\nTotal number of locales: 2\nTotal number of charmaps: 1",
+                          sys.stdout.getvalue())
+
+    # Req. 7: Option -v
+    def test_version_info_ignore_file_content(self):
+        test_args = ['locale.py', '-v', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args):
+            my_locale.main()
+            self.assertIn("Nicolas Huber\n25061944\nFriday 17 May 2024", sys.stdout.getvalue())
+
+    # Req. 8: Exclusive Options
+    def test_multiple_options(self):
+        test_args = ['locale.py', '-a', '-m', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args), self.assertRaises(SystemExit):
+            my_locale.main()
+            self.assertIn('Error: Invalid usage', sys.stdout.getvalue())
+
+    # Req. 9: Error Handling
+    def test_missing_language_argument(self):
+        test_args = ['locale.py', '-l', './data/locale_charmap_mixed.csv']
+        with patch('sys.argv', test_args):
+            with self.assertRaises(SystemExit) as cm:
+                my_locale.main()
+            self.assertIn("Incorrect usage for language info.", sys.stdout.getvalue())
+
 
 if __name__ == '__main__':
     unittest.main()
